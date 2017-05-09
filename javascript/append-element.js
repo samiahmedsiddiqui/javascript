8694bf7941b8614ad7d72c2ca32a855b8fee2ca2
Sami Ahmed Siddiqui <samiahmedsiddiqui@users.noreply.github.com>
2017-05-09 14:02:55 +0500

    Create append-element.js

---
+++ javascript/append-element.js
@@ -0,0 +1,5 @@
+script = document.createElement('script'); // Create Script Element
+script.type = 'text/javascript'; // Define type of the created Element
+script.src = 'js/single-player.js'; // Define the src of the Element
+
+head.appendChild(script); // Append the Created Script in the head of the page
